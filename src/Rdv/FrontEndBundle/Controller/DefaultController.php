de2fc5390c8ffdb7acc91d1f0228c51fa9973f94
Moussaoui <moussaoui.tarik.2015@gmail.com>
2016-10-27 12:55:39 +0200

    Add Template(asset)

--- src/Rdv/FrontEndBundle/Controller/DefaultController.php
+++ src/Rdv/FrontEndBundle/Controller/DefaultController.php
@@ -15,7 +15,6 @@ class DefaultController extends Controller
 
         $html =  $this->parsePlatform($html,$family);
 
-
         return $this->render('RdvFrontEndBundle:Default:index.html.twig' , [
             'html' => $html
         ]);
